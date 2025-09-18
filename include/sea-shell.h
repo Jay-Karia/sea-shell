f27dae1da1a029153e79cb738772c7de1ad68589
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-09-18 12:23:13 +0530

    feat: add required function like read_line, tokenize, and execute()

--- include/sea-shell.h
+++ include/sea-shell.h
@@ -4,9 +4,13 @@
 #define PROMPT "~>"
 #define HISTORY_LENGTH 1024
 #define LINE_BUFFER_SIZE 1024
+#define TOKEN_BUFFER_SIZE 64
+#define TOKEN_DELIMITERS " \t\r\n\a"
 
 void shell_loop(void);
 char* read_line(void);
-char** split_line(char* line);
+char** tokenize(char* line);
+int sea_shell_launch(char** args);
+int execute(char** args);
 
 #endif
--- utils/index.c
+++ utils/index.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "../include/sea-shell.h"
 
 void shell_loop(void)
 {
-	char* line;
-	char** args;
 	int status;
 
 	do {
-		printf("%s\n", PROMPT);
-		line = read_line();
-		args = split_line(line);
+		printf("%s", PROMPT);
+		char* line = read_line();
+		char** args = tokenize(line);
 		status = execute(args);
 
 		free(line);
@@ -42,19 +44,86 @@ char* read_line(void)
 		if (c == EOF || c == '\n') {
 			buffer[position] = '\0';
 			return buffer;
-		} else {
+		}
+		else {
 			buffer[position] = c;
 		}
 		position++;
-	}
 
-	// If buffer is exceeded, reallocate
-	if (position >= buffer_size) {
-		buffer_size += LINE_BUFFER_SIZE;
-		buffer = realloc(buffer, buffer_size);
-		if (!buffer) {
-			fprintf(stderr, "sea-shell: allocation error\n");
-			exit(EXIT_FAILURE);
+		// If buffer is exceeded, reallocate
+		if (position >= buffer_size) {
+			buffer_size += LINE_BUFFER_SIZE;
+			buffer = realloc(buffer, buffer_size);
+			if (!buffer) {
+				fprintf(stderr, "sea-shell: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
 		}
 	}
+}
+
+char** tokenize(char* line)
+{
+	int buffer_size = TOKEN_BUFFER_SIZE, position = 0;
+	char** token_buffer = malloc(buffer_size * sizeof(char*));
+	char* token;
+
+	if (!token_buffer) {
+		fprintf(stderr, "sea-shell: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, TOKEN_DELIMITERS);
+	while (token != NULL) {
+		token_buffer[position] = token;
+		position++;
+		if (position >= buffer_size) {
+			buffer_size += TOKEN_BUFFER_SIZE;
+			token_buffer = realloc(token_buffer, buffer_size * sizeof(char*));
+			if (!token_buffer) {
+				fprintf(stderr, "sea-shell: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, TOKEN_DELIMITERS);
+	}
+
+	token_buffer[position] = NULL;
+	return token_buffer;
+}
+
+int sea_shell_launch(char** args)
+{
+	pid_t pid, wpid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("sea-shell");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		perror("sea-shell");
+	}
+	else
+	{
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return 1;
+}
+
+int execute(char** args)
+{
+	if (args == NULL || args[0] == NULL) {
+		return 1;
+	}
+	return sea_shell_launch(args);
 }
