66399f1222340e60ce6a6b812e80afcc3925b371
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-10-02 12:56:18 +0530

    feat: add a single argument if quotes are used

--- utils/parser.c
+++ utils/parser.c
@@ -1,30 +1,72 @@
 #include "shell.h"
 
-char** parse_input(char* input)
+char **parse_input(char *input)
 {
     int bufsize = 64, position = 0;
-    char **tokens = malloc(bufsize * sizeof(char*));
-    char *token;
-
-    if (!tokens) {
+    char **tokens = malloc(bufsize * sizeof(char *));
+    
+    if (!tokens)
+    {
         fprintf(stderr, "shell: allocation error\n");
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(input, DELIMITERS);
-    while (token != NULL) {
-        tokens[position++] = token;
+    char *ptr = input;
+    char *start;
+    int in_quotes = 0;
 
-        if (position >= bufsize) {
+    while (*ptr)
+    {
+        // Skip whitespace
+        while (*ptr && strchr(DELIMITERS, *ptr) && !in_quotes)
+            ptr++;
+        if (!*ptr)
+            break;
+
+        start = ptr;
+
+        // Handle quoted strings
+        if (*ptr == '"' || *ptr == '\'')
+        {
+            char quote = *ptr;
+            ptr++;       // Skip opening quote
+            start = ptr; // Start after quote
+
+            // Find closing quote
+            while (*ptr && *ptr != quote)
+                ptr++;
+            if (*ptr == quote)
+            {
+                *ptr = '\0'; // Null terminate
+                ptr++;       // Skip closing quote
+            }
+
+            tokens[position++] = start;
+        }
+        else
+        {
+            // Regular token
+            while (*ptr && !strchr(DELIMITERS, *ptr))
+                ptr++;
+            if (*ptr)
+            {
+                *ptr = '\0';
+                ptr++;
+            }
+            tokens[position++] = start;
+        }
+
+        // Resize if needed
+        if (position >= bufsize)
+        {
             bufsize += 64;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens) {
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens)
+            {
                 fprintf(stderr, "shell: allocation error\n");
                 exit(EXIT_FAILURE);
             }
         }
-
-        token = strtok(NULL, DELIMITERS);
     }
     tokens[position] = NULL;
     return tokens;
